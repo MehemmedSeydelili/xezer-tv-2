9c804aeddb162f25d7f087c298fb9ae73f99cb0c
Ibrahim <i.qabilogl@gmail.com>
2022-09-27 22:30:41 +0400

    second-test

--- employee-ms/src/main/java/com/example/employeems/dao/entity/ExperienceEntity.java
+++ employee-ms/src/main/java/com/example/employeems/dao/entity/ExperienceEntity.java
@@ -1,7 +1,10 @@
 package com.example.employeems.dao.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "experiences")
 public class ExperienceEntity {
+
 }
