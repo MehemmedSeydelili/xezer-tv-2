cab5abb83fd1d53ae4ede2253dcc6f974523686f
Mahammad Seydalili <mahammadseydali>
2022-09-30 16:39:01 +0400

    Employee Entity

--- employee-ms/src/main/java/com/example/employeems/dao/entity/EmployeeEntity.java
+++ employee-ms/src/main/java/com/example/employeems/dao/entity/EmployeeEntity.java
@@ -2,21 +2,18 @@ package com.example.employeems.dao.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Setter
+@Getter
 @Table(name = "employees")
 public class EmployeeEntity {
 
@@ -40,18 +37,30 @@ public class EmployeeEntity {
     private Double salary;
 
     @CreationTimestamp
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @Column(name = "position_id")
-    private Long positionId;
 
-
-    /*@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    /*@ManyToOne
+    @JoinColumn(name = "position_id", referencedColumnName = "id")
     @JsonIgnore
     @ToString.Exclude
     private PositionEntity position;*/
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeEntity that = (EmployeeEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(birthDate, that.birthDate) && Objects.equals(email, that.email) && Objects.equals(address, that.address) && Objects.equals(salary, that.salary) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, birthDate, email, address, salary, createdAt, updatedAt);
+    }
 }
