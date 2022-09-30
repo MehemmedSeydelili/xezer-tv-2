641ba31dbad401d1234d2c140ee65cbba0253356
Ibrahim <i.qabilogl@gmail.com>
2022-09-30 15:44:55 +0400

    create ExprienceEntity class

--- employee-ms/src/main/java/com/example/employeems/dao/entity/ExperienceEntity.java
+++ employee-ms/src/main/java/com/example/employeems/dao/entity/ExperienceEntity.java
@@ -1,10 +1,55 @@
 package com.example.employeems.dao.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "experiences")
+@Getter
+@Setter
+@ToString
 public class ExperienceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String workplace;
+
+    private String position;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonBackReference
+    private EmployeeEntity employee;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
 }
