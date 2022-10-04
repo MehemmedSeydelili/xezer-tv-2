80679a4da88ef9608a1492502a0f48d6fba26e72
Mahammad Seydalili <mahammadseydali>
2022-10-04 18:06:28 +0400

    Employee Entity

--- employee-ms/src/main/java/com/example/employeems/model/view/EmployeeView.java
+++ employee-ms/src/main/java/com/example/employeems/model/view/EmployeeView.java
@@ -1,11 +1,9 @@
 package com.example.employeems.model.view;
 
-import com.example.employeems.dao.entity.ExperienceEntity;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Set;
 
 @Data
 public class EmployeeView implements Serializable {
@@ -15,6 +13,5 @@ public class EmployeeView implements Serializable {
     private final String mobilePhone;
     private final String address;
     private final Double salary;
-    private Set<ExperienceEntity> experiences;
 
 }
