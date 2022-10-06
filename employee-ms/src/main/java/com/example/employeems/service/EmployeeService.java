e1e289be95e04d4b0b5d28bbe5f9c2f9902a9459
Mahammad Seydalili <mahammadseydali>
2022-10-06 15:29:40 +0400

    update service package

--- employee-ms/src/main/java/com/example/employeems/service/EmployeeService.java
+++ employee-ms/src/main/java/com/example/employeems/service/EmployeeService.java
@@ -1,9 +1,8 @@
-package com.example.employeems.service.impl;
+package com.example.employeems.service;
 
 import com.example.employeems.model.dto.EmployeeDto;
 import com.example.employeems.model.view.EmployeeView;
 import com.example.employeems.repository.EmployeeRepository;
-import com.example.employeems.service.IEmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,33 +10,28 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class EmployeeService implements IEmployeeService{
+public class EmployeeService{
 
     private final EmployeeRepository employeeRepository;
 
-    @Override
     public List<EmployeeView> getAllEmployee() {
         return null;
     }
 
-    @Override
     public EmployeeView getEmployeeById(Long id) {
         /*employeeRepository.findById(id).orElseThrow(()-> new EmployeeNotFoundException
                 (String.format("Employee not found with id - %s", id)))*/
         return null;
     }
 
-    @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
         return null;
     }
 
-    @Override
     public EmployeeDto updatEmployee(Long id, EmployeeDto employeeDto) {
         return null;
     }
 
-    @Override
     public void deleteEmployee(Long id) {
 
     }
