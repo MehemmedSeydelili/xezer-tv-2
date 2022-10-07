b2a50967d5b22b241f61309c4109f2ec34090a67
Mahammad Seydalili <mahammadseydali>
2022-10-07 17:21:59 +0400

    update employee controller

--- employee-ms/src/main/java/com/example/employeems/controller/EmployeeController.java
+++ employee-ms/src/main/java/com/example/employeems/controller/EmployeeController.java
@@ -1,9 +1,46 @@
 package com.example.employeems.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.employeems.model.dto.EmployeeDto;
+import com.example.employeems.model.view.EmployeeView;
+import com.example.employeems.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
-@RequestMapping("/api/v1/employee")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    @GetMapping
+    public ResponseEntity<Set<EmployeeView>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(employeeService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeView> getById(@PathVariable("id") Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(employeeService.getEmployee(id));
+    }
+
+    @PostMapping
+        public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.createEmployee(employeeDto));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updpate(@PathVariable("id") Long id , @RequestBody EmployeeDto employeeDto){
+        return ResponseEntity.status(HttpStatus.OK).body(employeeService.updateEmployee(id,employeeDto));
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(Long id){
+        employeeService.deleteEmployee(id);
+    }
+
 }
+
