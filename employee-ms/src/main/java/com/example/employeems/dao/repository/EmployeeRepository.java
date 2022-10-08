7dd76d574b3efca76e79b3e8b59d45cb282c6872
Mahammad Seydalili <mahammadseydali>
2022-10-08 10:49:09 +0400

    update employee repository

--- employee-ms/src/main/java/com/example/employeems/dao/repository/EmployeeRepository.java
+++ employee-ms/src/main/java/com/example/employeems/dao/repository/EmployeeRepository.java
@@ -2,14 +2,18 @@ package com.example.employeems.dao.repository;
 
 import com.example.employeems.dao.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
-    Optional<String> findByfirstName(String name);
-    void deleteByfirstName(String name);
+    @Query(nativeQuery = true, value = "select * from employees where is_deleted false")
+    Set<EmployeeEntity> findAllByDeletedIsFalse();
+
+    Optional<EmployeeEntity> findByIdAndIsDeletedFalse(Long id);
 }
