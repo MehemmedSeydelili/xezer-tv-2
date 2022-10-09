19951877013f2f9d75bb207a6519ac16a764830b
Mahammad Seydalili <mahammadseydali>
2022-10-09 23:52:26 +0400

    adding gender field to dto

--- employee-ms/src/main/java/com/example/employeems/model/dto/EmployeeDto.java
+++ employee-ms/src/main/java/com/example/employeems/model/dto/EmployeeDto.java
@@ -1,36 +1,39 @@
 package com.example.employeems.model.dto;
 
 import com.example.employeems.dao.entity.EmployeeEntity;
+import com.example.employeems.model.enums.Gender;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.io.Serializable;
 import java.time.LocalDate;
 
 /**
  * A DTO for the {@link EmployeeEntity} entity
  */
 @Data
-public class EmployeeDto implements Serializable {
+public class EmployeeDto  {
 
     @NotNull(message = "First name can't be null")
-    @NotEmpty(message = "First name can't be empty")
-    @NotBlank
+    @NotBlank(message = "")
     private final String firstName;
 
-    @NotNull(message = "Last name can't be null")
-    @NotEmpty(message = "Last name can't be empty")
     @NotBlank
     private final String lastName;
 
+    private final Gender gender;
+
+    @NotNull(message = "Birth date can't be null")
     private final LocalDate birthDate;
 
     @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
     private final String email;
+
+    @NotNull(message = "Address can't be null")
     private final String address;
+
+    @NotNull(message = "Salary can't be null")
     private final Double salary;
-    private final Long positionId;
+
 }
