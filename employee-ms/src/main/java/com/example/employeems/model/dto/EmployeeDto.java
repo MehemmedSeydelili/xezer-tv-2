09172d8203b4788185dfda392c6e6345147287f8
Mahammad Seydalili <mahammadseydali>
2022-10-21 16:14:25 +0400

    update employee dto

--- employee-ms/src/main/java/com/example/employeems/model/dto/EmployeeDto.java
+++ employee-ms/src/main/java/com/example/employeems/model/dto/EmployeeDto.java
@@ -3,10 +3,12 @@ package com.example.employeems.model.dto;
 import com.example.employeems.dao.entity.EmployeeEntity;
 import com.example.employeems.model.enums.Gender;
 import lombok.Data;
+import org.springframework.format.annotation.NumberFormat;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.time.LocalDate;
 
 /**
@@ -15,13 +17,17 @@ import java.time.LocalDate;
 @Data
 public class EmployeeDto  {
 
+    @Null
+    private final Long id;
+
     @NotNull(message = "First name can't be null")
-    @NotBlank(message = "")
+    @NotBlank(message = "First name must contain at least one character")
     private final String firstName;
 
-    @NotBlank
+    @NotBlank(message = "Last name name must contain at least one character")
     private final String lastName;
 
+    @NotNull
     private final Gender gender;
 
     @NotNull(message = "Birth date can't be null")
@@ -30,10 +36,15 @@ public class EmployeeDto  {
     @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
     private final String email;
 
+    @NumberFormat(style = NumberFormat.Style.NUMBER)
+    private final String phoneNumber;
+
     @NotNull(message = "Address can't be null")
     private final String address;
 
     @NotNull(message = "Salary can't be null")
     private final Double salary;
+
+    private final Long positionId;
 
 }
